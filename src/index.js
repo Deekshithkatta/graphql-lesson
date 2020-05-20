aebec0bb48988f9f28b2dc4d34487a7979b30c01
Deekshithkatta <dxtkatta@gmail.com>
2020-05-20 22:19:21 +0530

    Successfully queried from first GraphQL query , commented for better understanding

--- src/index.js
+++ src/index.js
@@ -5,22 +5,57 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
-
+// GraphQl
 import {ApolloProvider} from 'react-apollo' // Access to state stored in Apollo
 import {createHttpLink} from 'apollo-link-http' // Connet to client (graphql)
 import {InMemoryCache} from 'apollo-cache-inmemory' //Cache for performance
-import {ApolloClient} from 'apollo-boost' //
+import {ApolloClient, gql} from 'apollo-boost' // Bundle of smaller libraries, gql function 
 
 import './index.css';
 import App from './App';
 
+// Link to GraphQL server
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+})
+
+//InMemoryCache
+const cache = new InMemoryCache()
+
+const client =  new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+
+
+client.query({
+  query: gql`
+  {
+    getCollectionsByTitle(title: "hats" ) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+  `
+}).then(response=> console.log(response))
+
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+     <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
